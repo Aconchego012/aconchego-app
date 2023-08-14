9f5fc93b240ba4731be872b80d572e8b772dea7e
Luan <luanrodrigues51@gmail.com>
2023-08-14 15:55:13 -0300

    Removes old code

--- gateway/src/app.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ClassesModule } from './classes/classes.module';
-import { PeopleModule } from './people/people.module';
-
-@Module({
-  imports: [ClassesModule, PeopleModule],
-})
-export class AppModule {}
